58a838775cd78c1d00bf35d52764ca460a4dccaf
miryala veronica <101985218+veronica17-web@users.noreply.github.com>
2022-09-29 16:05:58 +0530

    geturl

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -26,6 +26,31 @@ const createShortUrl = async function (req, res) {
  return res.status(201).send({status:true,data:data})
 
 }
+//==============================Get urlcode==========================================================
 
+
+const geturl = async function (req, res) {
+    try {
+        const urlCode = req.params.urlCode.trim()
+        if (!isValid(urlCode)) {
+            res.status(400).send({ status: false, message: 'Please provide valid urlCode' })
+        }
+
+        const url = await urlModel.findOne({ urlCode: urlCode })   //check in Db
+
+        if (!url) {
+            return res.status(404).send({ status: false, message: 'No URL Found' })
+        }
+
+        return res.status(302).redirect(url.longUrl)
+
+    } catch (err) {
+
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+
+module.exports.geturl = geturl
 
 module.exports = { createShortUrl }
