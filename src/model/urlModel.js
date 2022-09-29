3c115c77bf15ba1638eeaa0b01f53e1048b52581
kusum <kushay317@gmail.com>
2022-09-29 12:18:50 +0530

    modelscreated

--- src/model/urlModel.js
+++ src/model/urlModel.js
@@ -1,1 +1,24 @@
 const mongoose = require("mongoose")
+const urlSchema = new mongoose.schema({
+    longUrl: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    urlCode: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        trim: true,
+    },
+
+    shortUrl: {
+        type: String,
+        required: true,
+        unique: true,
+    }
+    }, { timestamps: true })
+ 
+module.exports = mongoose.model('url', urlSchema)
+
